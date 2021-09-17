9914f1d3b882028e0e27e09291d2008ca0cac641
mindym1 <76831690+mindy-m@users.noreply.github.com>
2021-09-17 02:50:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,48 @@
+# Part 1 A -- Make a Line
+
+def make_line(size):
+   line = ""
+   for i in range(size):
+      line += "#"
+   return line
+
+print(make_line(5))
+
+# Part 1 B -- Make a Square
+# create a function using your make_line function to code a square
+
+
+
+
+
+# Part 1 C -- Make a Rectangle
+
+
+
+
+
+# Part 2 A -- Make a Stairs
+
+
+
+
+
+# Part 2 B -- Make Space-Line 
+
+
+
+
+
+# Part 2 C -- Make Isosceles Triangle
+
+
+
+
+
+# Part 3 -- Make a Diamond
+
+
+
+
+
+
